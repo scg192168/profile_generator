1b65c1c65d2d3845bc707b0a44f1f6e014756e0d
Shyamal <scghoshbc@gmail.com>
2023-09-04 18:44:33 -0700

    challenge completed

---
+++ survey.js
@@ -0,0 +1,23 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("What's your name? Nicknames are also acceptable :) ", (name) => {
+  rl.question("What's an activity you like doing? ", (activity) => {
+    rl.question("What do you listen to while doing that? ", (music) => {
+      rl.question("Which meal is your favourite (eg: dinner, brunch, etc.)? ", (meal) => {
+        rl.question("What's your favourite thing to eat for that meal? ", (food) => {
+          rl.question("Which sport is your absolute favourite? ", (sport) => {
+            rl.question("What is your superpower? In a few words, tell us what you are amazing at! ", (superpower) => {
+              console.log(`My name is ${name}. I love ${activity} and listening to ${music}. My favorite meal of the day is ${meal}, and my favorite food for that is ${food}. I'm a huge fan of ${sport} and my superpower is ${superpower}.`);
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
+});
